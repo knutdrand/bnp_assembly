767f1acecd978abe3c40e1f162652377de73caaf
Knut Rand <knutdrand@gmail.com>
2023-04-25 15:37:44 +0200

    Passing count test

--- bnp_assembly/hic_distance_matrix.py
+++ bnp_assembly/hic_distance_matrix.py
@@ -4,26 +4,29 @@ from bnp_assembly.location import LocationPair, Location
 
 
 def calculate_distance_matrices(contig_dict: tp.Dict[str, int], location_pairs: LocationPair, window_size=15):
+    return count_window_combinastions(contig_dict, location_pairs)
+
+
+def count_window_combinastions(contig_dict: tp.Dict[str, int], location_pairs: LocationPair, window_size=15) -> Counter:
     overlap_counts = Counter()
     inside_counts = Counter()
     for a, b in zip(location_pairs.location_a, location_pairs.location_b):
         for direction_a in ('l', 'r'):
             for direction_b in ('l', 'r'):
-                if direction_a=='l':
+                if direction_a == 'l':
                     offset_a = a.offset
                 else:
                     offset_a = contig_dict[int(a.contig_id)]-a.offset
-                if direction_b=='l':
+                if direction_b == 'l':
                     offset_b = b.offset
                 else:
                     offset_b = contig_dict[int(b.contig_id)]-b.offset
-            id_a = (int(a.contig_id), direction_a)
-            id_b = (int(b.contig_id), direction_b)
-            #Case: overlap_count
-            if (a.contig_id != b.contig_id) and (offset_b<window_size) and (offset_b<window_size):
-
-                overlap_counts[frozenset((id_a, id_b))] += 1
-            elif id_a == id_b:
-                if min(offset_a, offset_b)<window_size and  window_size<=max(offset_a, offset_b)<2*window_size:
-                    inside_counts[id_a] + 1
+                id_a = (int(a.contig_id), direction_a)
+                id_b = (int(b.contig_id), direction_b)
+                if (a.contig_id != b.contig_id) and (offset_a < window_size) and (offset_b<window_size):
+                    overlap_counts[frozenset((id_a, id_b))] += 1
+                
+                elif id_a == id_b:
+                    if min(offset_a, offset_b) < window_size and window_size <= max(offset_a, offset_b) < 2*window_size:
+                        inside_counts[id_a] += 1
     return overlap_counts, inside_counts
--- tests/test_hic_simulation_counts.py
+++ tests/test_hic_simulation_counts.py
@@ -1,11 +1,12 @@
 import pytest
 from bnp_assembly.location import LocationPair, Location
-from bnp_assembly.hic_distance_matrix import calculate_distance_matrices
+from bnp_assembly.hic_distance_matrix import calculate_distance_matrices, count_window_combinastions
 
 
 @pytest.fixture
 def contig_list():
     return {0: 100, 1: 50}
+
 
 @pytest.fixture
 def location_pairs():
@@ -20,6 +21,12 @@ def location_pairs():
         (0, 20)])
     return LocationPair(location_a, location_b)
 
+def test_count_window_combinations(contig_list, location_pairs):
+    overlap_count, inside_counts = count_window_combinastions(contig_list, location_pairs)
+    print(overlap_count)
+    assert overlap_count[frozenset(((0, 'l'), (1, 'r')))] == 2
+    assert sum(overlap_count.values()) == 2
+    assert inside_counts[(0, 'l')]==1
 
 def test_distance_acceptance(contig_list, location_pairs):
     graph = calculate_distance_matrices(contig_list, location_pairs)
