ca7d6e517be1c40d0730f83b35526b0e8349b711
Knut Rand <knutdrand@gmail.com>
2023-04-27 10:45:03 +0200

    typing

--- bnp_assembly/scaffold.py
+++ bnp_assembly/scaffold.py
@@ -1,8 +1,8 @@
 from .hic_distance_matrix import calculate_distance_matrices
 from .path_finding import best_path
+from .location import LocationPair
 
-
-def scaffold(contig_dict: dict, read_pairs, window_size=15):
+def scaffold(contig_dict: dict, read_pairs: LocationPair, window_size=15):
     distance_matrix = calculate_distance_matrices(contig_dict, read_pairs, window_size=window_size)
     path = best_path(distance_matrix)
     return path
