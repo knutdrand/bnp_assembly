f4bc1f49d5b0ed7c50ec7442c71690a9d64bf032
Ivar Grytten <ivar.grytten@gmail.com>
2023-05-23 10:03:41 +0200

    Agp support

--- bnp_assembly/cli.py
+++ bnp_assembly/cli.py
@@ -1,9 +1,13 @@
 """Console script for bnp_assembly."""
+import os
+
 # todo
 import numpy as np
 
 import typer
 import bionumpy as bnp
+
+from bnp_assembly.agp import ScaffoldAlignments
 from .io import get_read_pairs, get_genomic_read_pairs
 from .path_finding import best_path, PathFinder
 from .hic_distance_matrix import calculate_distance_matrices
@@ -22,6 +26,7 @@ def scaffold(contig_file_name: str, read_filename: str, out_file_name: str, thre
     >>> main()
 
     '''
+    out_directory = os.path.sep.join(out_file_name.split(os.path.sep)[:-1])
     genome = bnp.Genome.from_file(contig_file_name)
     encoding = genome.get_genome_context().encoding
     contig_dict= genome.get_genome_context().chrom_sizes
@@ -33,9 +38,14 @@ def scaffold(contig_file_name: str, read_filename: str, out_file_name: str, thre
     out_names = []
     out_sequences = []
 
+    alignments = []
+
     for i, path in enumerate(paths):
         sequences = []
+        scaffold_name = f'scaffold{i}_'+':'.join(f'{dn.node_id}{dn.orientation}' for dn in path.directed_nodes)
+        offset = 0
         for j, (contig_id, is_reverse) in enumerate(path.to_list()):
+
             if j > 0:
                 # adding 200 Ns between contigs
                 sequences.append(bnp.as_encoded_array('N' * 200, bnp.encodings.ACGTnEncoding))
@@ -44,7 +54,13 @@ def scaffold(contig_file_name: str, read_filename: str, out_file_name: str, thre
                 seq = bnp.sequence.get_reverse_complement(seq)
             sequences.append(bnp.change_encoding(seq, bnp.encodings.ACGTnEncoding))
 
-        out_names.append(f'scaffold{i}_'+':'.join(f'{dn.node_id}{dn.orientation}' for dn in path.directed_nodes))
+            alignments.append(
+                    (scaffold_name, offset+1, offset+len(sequences[-1]),
+                     str(contig_id), 1, len(sequences[-1]), "+" if not is_reverse else "-")
+            )
+            offset = sum((len(s) for s in sequences))
+
+        out_names.append(scaffold_name)
         out_sequences.append(np.concatenate(sequences))
     for path in paths:
         logging.info(path.directed_nodes)
@@ -53,9 +69,12 @@ def scaffold(contig_file_name: str, read_filename: str, out_file_name: str, thre
             zip(out_names, out_sequences)
         ))
 
+    alignments = ScaffoldAlignments.from_entry_tuples(alignments)
+    alignments.to_agp(out_directory + "/scaffolds.agp")
+
 
 @app.command()
-def heatmap(fasta_filename: str, interval_filename: str, out_file_name: str, bin_size: int = 0):
+def heatmap(fasta_filename: str, interval_filename: str, agp_file: str, out_file_name: str, bin_size: int = 0):
     genome = bnp.Genome.from_file(fasta_filename, filter_function=None)
     if bin_size == 0:
         bin_size = max(1000, genome.size // 1000)
@@ -63,6 +82,20 @@ def heatmap(fasta_filename: str, interval_filename: str, out_file_name: str, bin
     locations_pair = get_genomic_read_pairs(genome, interval_filename)
     interaction_matrix = InteractionMatrix.from_locations_pair(locations_pair, bin_size=bin_size)
     fig = interaction_matrix.plot()
+
+    # add contig ids from agp file
+    global_offset = genome.get_genome_context().global_offset
+    alignments = ScaffoldAlignments.from_agp(agp_file)
+    scaffold_offsets = global_offset.get_offset(alignments.scaffold_id)
+    contig_offsets = (scaffold_offsets + alignments.scaffold_start) // bin_size
+    fig.update_layout(xaxis=dict(tickmode='array', tickvals=contig_offsets, ticktext=alignments.contig_id.tolist())),
+    fig.update_xaxes(
+        showgrid=True,
+        ticks="outside",
+        tickson="boundaries",
+        ticklen=20
+    )
+
     fig.show()
     fig.write_image(out_file_name)
     interaction_matrix.normalize_matrix().plot().show()
---
+++ tests/test_agp.py
@@ -0,0 +1,6 @@
+import pytest
+from bnp_assembly.agp import ScaffoldAlignments
+
+
+def test_agp():
+    pass
