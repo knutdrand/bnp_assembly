d751dfe9caaba93e688035084c05a6a97caab882
Ivar Grytten <ivar.grytten@gmail.com>
2023-09-21 15:19:37 +0200

    initial skeleton for dynamic heatmap creation

--- bnp_assembly/bnp_assembly/pre_sampled_dynamic_heatmap_comparison.py
+++ bnp_assembly/bnp_assembly/pre_sampled_dynamic_heatmap_comparison.py
@@ -4,11 +4,24 @@ Run through all reads for inter to calculate F = dynamic bin size heatmap
 Compare edge F's to F's calulated from sampled intra reads.
 * Find wich distance matches each bin best and do median or something
 '''
-from typing import Tuple
-
+from typing import Tuple, Callable
 import numpy as np
-
+from dataclasses import dataclass
 from bnp_assembly.input_data import FullInputData
+from bnp_assembly.location import LocationPair
+
+
+@dataclass
+class DynamicHeatmapConfig:
+    scale_func: Callable[[int], int] = lambda x: np.log(x+1)
+    inverse_scale_func: Callable[[int], int] = lambda x: np.exp(x) - 1
+    n_bins: int = 100
+
+    @property
+    def max_distance(self):
+        return self.inverse_scale_func(self.n_bins)-1
+
+log_config = DynamicHeatmapConfig(n_bins=10)
 
 
 class DynamicHeatmap:
@@ -21,6 +34,13 @@ class DynamicHeatmap:
         return self._array
 
     @classmethod
+    def empty(cls, config: DynamicHeatmapConfig):
+        return cls(np.zeros((config.n_bins, config.n_bins)), config.scale_func)
+
+    def add(self, a, b):
+        self._array[self._scale_func(a), self._scale_func(b)] += 1
+
+    @classmethod
     def create_from_positions(cls, positions_tuple: Tuple[list, list], scale_func=lambda x: np.log(x + 1),
                               n_bins: int = 100):
         scaled_positions = tuple(scale_func(p) for p in positions_tuple)
@@ -30,6 +50,11 @@ class DynamicHeatmap:
         indices = a * n_bins + b
         array = np.bincount(indices, minlength=n_bins ** 2).reshape(n_bins, n_bins)
         return cls(array, scale_func)
+
+
+class DynamicHeatmaps:
+    """Represents multiple DynamicHeatmaps in a shared np.array"""
+    pass
 
 
 def get_heatmaps_for_edges(input_data, max_distance, n_bins, n_precomputed):
@@ -52,3 +77,49 @@ def method(input_data: FullInputData, max_distance, n_bins, n_precomputed):
                             range(n_precomputed)}
     pre_sampled_heatmaps = np.array([pre_sampled_heatmaps[d] for d in range(n_precomputed)])
     heatmaps_for_edges = get_heatmaps_for_edges(input_data, max_distance, n_bins, n_precomputed)
+
+
+
+class PreComputedDynamicHeatmapCreator:
+    """
+    Precomputes dynamic heatmaps by using reads from suitable contigs
+    * Finds contigs that are large enough to use for estimation
+    * Iterates possible gaps between contigs
+    * Emulates two smaller contig on chosen contigs and uses these to estimate counts
+    """
+    def __init__(self, input_data: FullInputData, heatmap_config: DynamicHeatmapConfig = log_config, n_precomputed: int = 10):
+        self._input_data = input_data
+        self._gap_distances = np.array([2 ** (d + 1) for d in range(n_precomputed)])
+        self._config = heatmap_config
+        self._contig_sizes = input_data.genome.get_genome_context().chrom_sizes
+        self._chosen_contigs = self._get_suitable_contigs_for_estimation()
+
+    def _get_suitable_contigs_for_estimation(self):
+        # find contigs that are at least 2 x max distance
+        contigs = [contig for contig, size in self._contig_sizes.items() if size >= 2 * self._config.max_distance]
+        return contigs
+
+    @staticmethod
+    def add_contig_offset_pair_to_heatmap(heatmap: DynamicHeatmap, offset_a, offset_b, gap: int, contig_size: int):
+        split_position = contig_size // 2
+        assert offset_a < split_position
+        assert offset_b >= split_position
+        heatmap_offset_a = (split_position - gap) - offset_a - 1
+        heatmap_offset_b = offset_b - (split_position + gap)
+        assert heatmap_offset_a >= 0
+        assert heatmap_offset_b >= 0
+        heatmap.add(heatmap_offset_a, heatmap_offset_b)
+
+    def create_for_gap_distance(self, gap_distance: int) -> DynamicHeatmap:
+        heatmap = DynamicHeatmap.empty(self._config)
+        read_pairs = next(self._input_data.paired_read_stream)
+        for pair in read_pairs:
+            if (pair.location_a.contig_id == pair.location_b.contig_id
+                    and pair.location_a.contig_id in self._chosen_contigs):
+                self.add_read_pair_to_heatmap(heatmap, pair, gap_distance)
+
+    def create(self) -> DynamicHeatmaps:
+        for bin, gap in enumerate(self._gap_distances):
+            heatmap = self.create_for_gap_distance(gap)
+
+
--- bnp_assembly/tests/test_pre_sampled_dynamic_heatmap_comparison.py
+++ bnp_assembly/tests/test_pre_sampled_dynamic_heatmap_comparison.py
@@ -1,7 +1,9 @@
 import numpy as np
 from numpy.testing import assert_array_equal
 
-from bnp_assembly.pre_sampled_dynamic_heatmap_comparison import DynamicHeatmap
+from bnp_assembly.location import LocationPair, Location
+from bnp_assembly.pre_sampled_dynamic_heatmap_comparison import DynamicHeatmap, PreComputedDynamicHeatmapCreator
+
 a = np.array([1,2,3,4,5,6,7,8,9,10])
 b = np.array([2,4,6,8,10,12,14,16,18,20])
 
@@ -12,3 +14,16 @@ def test_create_from_positions():
     assert_array_equal(dh.array, np.array([[0, 0, 0], [0, 0, 1], [0, 0, 0]]))
     dh = DynamicHeatmap.create_from_positions((a, b), n_bins=5, scale_func=lambda x: x)
     assert_array_equal(dh.array, np.array([[0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0 ,0, 1], [0, 0, 0, 0, 0], [0, 0, 0, 0 ,0]]))
+
+
+def test_add_sampled_read_pair_to_heatmap():
+    c = PreComputedDynamicHeatmapCreator
+    heatmap = DynamicHeatmap(np.zeros((10, 10)), lambda x: x)
+    c.add_contig_offset_pair_to_heatmap(heatmap, 2, 6, gap=0, contig_size=10)
+    assert np.sum(heatmap.array) == 1
+    assert heatmap.array[2, 1] == 1
+    c.add_contig_offset_pair_to_heatmap(heatmap, 0, 5, gap=0, contig_size=10)
+    assert np.sum(heatmap.array) == 2
+    assert heatmap.array[4, 0] == 1
+
+
